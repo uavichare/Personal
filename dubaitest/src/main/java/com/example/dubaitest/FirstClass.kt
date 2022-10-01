b91aee4faf2b08a696bfdadd2113f5a8050da370
dilipgupta123-123 <GDIlip10#$%>
2022-10-02 01:07:55 +0530

    jkj

---
+++ dubaitest/src/main/java/com/example/dubaitest/FirstClass.kt
@@ -0,0 +1,17 @@
+package com.example.dubaitest
+
+import android.os.Bundle
+import com.indooratlas.android.sdk.IALocation
+import com.indooratlas.android.sdk.IALocationListener
+
+class FirstClass :IALocationListener {
+    override fun onLocationChanged(p0: IALocation?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+        TODO("Not yet implemented")
+    }
+
+
+}
---
+++ dubaitest/src/main/java/com/example/dubaitest/TestClass.kt
@@ -0,0 +1,12 @@
+package com.example.dubaitest
+
+import android.content.Context
+import android.widget.Toast
+
+class TestClass {
+
+    fun callTest(context:Context)
+    {
+        Toast.makeText(context,"done",Toast.LENGTH_LONG).show()
+    }
+}
